dbfb9cc182e48178743d68e13a410115a0cde72a
Thrivikram Mudunuri <23713701+tmudunuri@users.noreply.github.com>
2023-03-26 13:45:58 -0400

    Handle tuit likes

--- src/tuiter/tuits-list/tuit-stats.js
+++ src/tuiter/tuits-list/tuit-stats.js
@@ -1,18 +1,27 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { likeTuit } from "../tuits/tuits-reducer";
 
 const TuitStats = ({ tuit }) => {
+    const dispatch = useDispatch();
+    const likeTuitHandler = (id) => {
+        dispatch(likeTuit(id));
+    }
+
     return (
         <div className="row">
             <div className="col">
-                <i className="fa-regular fa-comment pe-2"></i>{tuit.replies}
+                <i className="fa-regular fa-comment pe-2"></i>
+                <span className="text-muted">{tuit.replies}</span>
             </div>
             <div className="col">
-                <i className="fa-solid fa-retweet pe-2"></i>{tuit.retuits}
+                <i className="fa-solid fa-retweet pe-2"></i>
+                <span className="text-muted">{tuit.retuits}</span>
             </div>
             <div className="col">
-                {tuit.liked ?
-                    <i className="fa-solid fa-heart text-danger pe-2"></i> : <i className="fa-regular fa-heart pe-2"></i>}
-                {tuit.likes}
+                {tuit.liked ? <i className="fa-solid fa-heart text-danger pe-2" onClick={() => likeTuitHandler(tuit._id)}></i>
+                    : <i className="fa-regular fa-heart pe-2" onClick={() => likeTuitHandler(tuit._id)}></i>}
+                <span className="text-muted">{tuit.likes}</span>
             </div>
             <div className="col">
                 <i className="fa-solid fa-share-nodes"></i>
--- src/tuiter/tuits/tuits-reducer.js
+++ src/tuiter/tuits/tuits-reducer.js
@@ -22,6 +22,11 @@ const tuitsSlice = createSlice({
     name: "tuits",
     initialState: tuits,
     reducers: {
+        likeTuit(state, action) {
+            const tuit = state.find(tuit => tuit._id === action.payload);
+            tuit.liked = !tuit.liked;
+            tuit.likes += tuit.liked ? 1 : -1;
+        },
         deleteTuit(state, action) {
             const index = state
                 .findIndex(tuit =>
@@ -38,5 +43,5 @@ const tuitsSlice = createSlice({
     }
 })
 
-export const { createTuit, deleteTuit } = tuitsSlice.actions;
+export const { createTuit, deleteTuit, likeTuit } = tuitsSlice.actions;
 export default tuitsSlice.reducer;
